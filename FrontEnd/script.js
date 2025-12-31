b88d8e86a1201e4fa482615aabdb74685f4ea28b
Vincez <vincentprobst45@gmail.com>
2025-12-31 15:45:39 +0100

    Api get travaux et filtres

---
+++ FrontEnd/script.js
@@ -0,0 +1,42 @@
+
+async function getWorks(){
+  const response = await fetch("http://localhost:5678/api/works", {
+    method : "get",
+    headers :{
+        "Content-Type" : "application/json"
+    }})
+    const works = await response.json()
+    return works
+}
+async function displayWorks(paramWorks) {
+    let htmlString = ''
+    const works = [...paramWorks]
+        works.forEach(work => {
+            htmlString = htmlString.concat(`<figure data-category="${work.category.name}">
+                <img src="${work.imageUrl}" alt="${work.title}">
+                <figcaption>${work.title}</figcaption>
+                </figure>`)
+        });
+    document.getElementById('portfolio').querySelector('.gallery').innerHTML = htmlString
+}
+function filterByCategory(category){
+    if(category != "Tous")
+    {
+        const filteredWorks = works.filter((work) => work.category.name === category)
+        displayWorks(filteredWorks)
+    } else{
+        displayWorks(works)
+    }
+}
+let works = []
+async function firstDisplay(){
+    works = await getWorks()
+    displayWorks(works) 
+}
+firstDisplay()
+
+document.querySelectorAll(".filters-container button").forEach((button) => {button.addEventListener("click", (e) => {
+        let text = e.target.innerText || e.target.textContent;
+        filterByCategory(text)
+    })
+})
